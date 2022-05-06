df368b957966fbdc4d7f83025f1c92d7705e7d92
trondongetacademy <97169276+trondongetacademy@users.noreply.github.com>
2022-05-06 09:59:30 +0200

    Add files via upload

---
+++ KrokodilleSpillet/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace KrokodilleSpillet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            bool status = true;
+            int win = 0;
+            int tap = 0;
+
+
+
+            while (status)
+            {
+                Random rnd = new Random();
+                int number1 = rnd.Next(1, 11);
+                int number2 = rnd.Next(1, 11);
+
+                Console.WriteLine("The Crocodile game!");
+                Console.WriteLine("Krokodille: " + number1 + "_" + number2);
+                Console.WriteLine("Velg mellom > | = | <");
+
+                var brukerValg = Console.ReadLine();
+                
+
+                Console.WriteLine(number1 + brukerValg + number2);
+
+                Console.WriteLine("Du valgte: " + brukerValg);
+                Console.WriteLine("Krokodille: ");
+                if (brukerValg != ">" && brukerValg != "=" && brukerValg != "<")
+                {
+                    Console.WriteLine("Ugyldig tegn");
+                    status = false;
+                }
+                else
+                {
+                    
+                    if (brukerValg == ">" && number1 > number2) win++;
+                    if (brukerValg == "=" && number1 > number2) tap++;
+                    if (brukerValg == "<" && number1 > number2) tap++;
+
+                    if (brukerValg == "=" && number1 == number2) win++;
+                    if (brukerValg == ">" && number1 == number2) tap++;
+                    if (brukerValg == "< " && number1 == number2) tap++;
+
+                    if (brukerValg == "<" && number1 < number2) win++;
+                    if (brukerValg == ">" && number1 < number2) tap++;
+                    if (brukerValg == "=" && number1 < number2) tap++;
+                    
+
+                    Console.WriteLine("seier: " + win);
+                    Console.WriteLine("tap: " + tap);
+                    if (win == 10 || tap == 10)
+                    {
+                        if (tap == 10) Console.WriteLine("Du tapte!");
+                        status = false;
+                        if (win == 10) Console.WriteLine("Du vant!");
+                    }
+                }
+            }
+        }
+    }
+}
